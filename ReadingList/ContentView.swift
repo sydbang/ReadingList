c8e23bfe5188416f974a4ec3923e38a8ec873bff
sydbang <sydney.bang94@gmail.com>
2022-01-26 15:25:12 -0500

    Initial Commit

---
+++ ReadingList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReadingList
+//
+//  Created by Sunghee Bang on 2022-01-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReadingList/ReadingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingListApp.swift
+//  ReadingList
+//
+//  Created by Sunghee Bang on 2022-01-26.
+//
+
+import SwiftUI
+
+@main
+struct ReadingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
